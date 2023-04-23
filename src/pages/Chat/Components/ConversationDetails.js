1bd36a7fdc381639922433f642882a47f1eb552b
kobir1989 <kobir.h.ritu@gmail.com>
2023-04-23 21:33:44 +0600

    Fixing a conversation update onsubmit issue

--- src/pages/Chat/Components/ConversationDetails.js
+++ src/pages/Chat/Components/ConversationDetails.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Button from '../../../components/common/Button/Button';
 import Typography from '../../../components/common/Typography/Typography';
 import Message from './Message';
@@ -7,26 +7,37 @@ import styles from '../styles/ConversationDetails.module.scss';
 import { Context } from '../../../store/Context';
 import { useHttpHook } from '../../../hooks/useHttpHook';
 
-const ConversationDetails = ({ messages, loading, error, roomId }) => {
+const ConversationDetails = ({ messages = [], loading, error, roomId }) => {
+  console.log(messages)
   const [text, setText] = useState('')
-  const [serverResponse, setServerResponse] = useState(null)
+  const [isEror, setIsError] = useState(null)
   const { state } = useContext(Context);
   const { authToken } = state;
   const { userPayload } = authToken;
-
   const findSender = messages.find(msg => msg?.chatRoom === roomId);
 
+  //onChage Handler 
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+    if (e.target.value !== '') {
+      setIsError(null)
+    }
+  }
   //this function will hold the response sent from server.
   const getResponseData = (data) => {
-    setServerResponse(data)
+    if (data) {
+      messages.push(data?.newMessage)
+    }
   }
-
   //custom hook to send request to server 
   const { sendRequest, loading: textSubmitLoading } = useHttpHook()
 
   //onSubmit handler 
   const handleTextMessage = (e) => {
     e.preventDefault()
+    if (!roomId) {
+      return setIsError('Please choose a person to engage in conversation!')
+    }
     const data = {
       message: text,
       sender: userPayload?._id,
@@ -43,7 +54,7 @@ const ConversationDetails = ({ messages, loading, error, roomId }) => {
   return (
     <div className={styles.conversation_details}>
       {/* userDetails */}
-      <div className={styles.message_sender_details}>
+      {roomId && <div className={styles.message_sender_details}>
         <img src={findSender?.sender?.image || "/assets/user.png"} alt="user" />
         <div className={styles.sender_info}>
           <Typography variant='smBold700' color="primary">
@@ -53,18 +64,24 @@ const ConversationDetails = ({ messages, loading, error, roomId }) => {
             {findSender?.sender?.email}
           </Typography>
         </div>
-      </div>
+      </div>}
 
       {/* messages */}
       <div className={styles.message_list}>
         {!loading && !error && messages?.length > 0 ? messages.map((message) => (
           <Message
-            isAdmin={message?.sender?._id === userPayload?._id ? true : false}
+            isAdmin={message?.sender?._id === userPayload?._id || message?.sender === userPayload?._id ? true : false}
             message={message?.message}
             key={message?._id}
             time={message?.createdAt}
           />
-        )) : null}
+        )) :
+          <div className={styles.not_found_message}>
+            {!loading && !error && <Typography variant='body' color='red'>
+              Please choose a person to engage in conversation.
+            </Typography>}
+          </div>
+        }
 
       </div>
       {/* form */}
@@ -76,7 +93,8 @@ const ConversationDetails = ({ messages, loading, error, roomId }) => {
             required name='message'
             autoComplete='off'
             value={text}
-            onChange={(e) => { setText(e.target.value) }}
+            onChange={handleTextChange}
+            className={isEror ? styles.input_error_styles : ''}
           />
           <Button
             disabled={textSubmitLoading}
@@ -85,6 +103,7 @@ const ConversationDetails = ({ messages, loading, error, roomId }) => {
           >
             <Icons name='send' color='#9fa7b6' />
           </Button>
+          {isEror && <div className={styles.error_message_wrapper}>  <Typography variant='small' color='red'>{isEror}</Typography></div>}
         </form>
       </div>
     </div>
