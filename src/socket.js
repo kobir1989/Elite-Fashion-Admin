0da1fcc42631305a36e55de38852570a53d1cf62
kobir1989 <kobir.h.ritu@gmail.com>
2023-05-12 15:36:07 +0600

    Fix: socket.io server URL issue

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const URL = process.env.REACT_APP_URL_MAIN
+const URL = process.env.REACT_APP_BASE_URL
 export const socket = io(URL, {
    reconnectionDelay: 1000,
    reconnection: true,
